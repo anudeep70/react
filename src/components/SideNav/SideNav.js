16ed853b40dc91b6713b576d0dd543ee03e3b753
anudeep chakilam <anudeeppy>
2020-06-18 11:40:48 +0530

    basic portfolio

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './sidenav.css';
+
+export const SideNav = ({ handleSectionClick }) => {
+    return (
+        <div className="sidenav">
+            <img
+                src="https://placeimg.com/320/320/people"
+                className="avatar"
+                alt="avatar"
+            />
+            <h2 className="main-name">Anudeep</h2>
+            <p className="subtitle">"Aspire to inspire"</p>
+            <div className="sections-list">
+                <p className="section-list-element" onClick={() => handleSectionClick("about")}>About Me</p>
+                <p className="section-list-element" onClick={() => handleSectionClick("skills")}>Skills</p>
+                <p className="section-list-element" onClick={() => handleSectionClick("projects")}>Projects</p>
+            </div>
+        </div>
+    );
+};
---
+++ src/constants/Skills.js
@@ -0,0 +1,28 @@
+export const skills = {
+    heading: "Skills",
+    quote: "Build your skills, not your resume",
+    techSkills: [
+        "JavaScript",
+        "ReactJS",
+        "C++",
+        "C",
+        "Java",
+        "Python",
+        "HTML",
+        "CSS",
+        "jQuery",
+        "Bootstrap"
+    ],
+    industryKnowledge: [
+        "Frontend",
+        "Backend",
+        "Web",
+    ],
+    interpersonalSkills: [
+        "Communication",
+        "Teaching",
+        "Time Management",
+        "Leadership",
+        "Management"
+    ]
+};
